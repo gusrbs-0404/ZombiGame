114020a361568da8246a9df6bdcb0b3f7cb84a6a
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-11 16:10:58 +0900

    Update Singleton Pattern

--- src/ZombieGame/ZombieGameSystem.java
+++ src/ZombieGame/ZombieGameSystem.java
@@ -1,10 +1,25 @@
 package ZombieGame;
 
-public class ZombieGameSystem {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ZombieGameSystem {
+	private static Scanner scan = new Scanner(System.in);
+	
+	private static Human human = new Human();
+	private static Normal normal = new Normal();
+	private static Boss boss = new Boss();
+	
+	private void ZombieGameSystem()	{
 
 	}
-
+	
+	private static ZombieGameSystem instance = new ZombieGameSystem();
+	
+	public static ZombieGameSystem getInstance() {
+		return instance;
+	}
+	
+	public static void run() {
+		
+	}
 }
