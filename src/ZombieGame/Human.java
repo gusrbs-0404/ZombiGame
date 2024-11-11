8fcb312f332c465e8e061e621082d77e5fd835f9
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-11 16:45:11 +0900

    Update potion() 완성

--- src/ZombieGame/Human.java
+++ src/ZombieGame/Human.java
@@ -1,6 +1,6 @@
 package ZombieGame;
 
-public class Human extends Unit  implements Attack{
+public class Human extends Unit  implements Attack, Potion{
 
 	Human() {
 		super("길동", 150);
@@ -9,6 +9,28 @@ public class Human extends Unit  implements Attack{
 	@Override
 	public void attack(Unit unit) {
 		System.out.println("깡!!!!");
+	}
+
+	@Override
+	public void potion(Human human) {
+		if (human instanceof Human) {
+			Human target = (Human) human;
+			
+			int ranNum = ZombieGameSystem.ranHp();
+			
+			while (target.hp < target.MAX_HP) {
+				System.out.println(target + "|| [HP] 회복중");
+				target.hp++;
+				ranNum --;
+				try {
+					Thread.sleep(300);
+				} catch (Exception e) {
+				}
+			}
+
+
+			System.out.println("<<< 회복 완료 >>> ");
+		}
 	}
 
 }
