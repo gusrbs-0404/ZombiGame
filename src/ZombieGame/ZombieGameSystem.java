5daf1f95b7f732e5ee79871dd37f804e2d5ef826
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-11 18:03:28 +0900

    Update menu()

--- src/ZombieGame/ZombieGameSystem.java
+++ src/ZombieGame/ZombieGameSystem.java
@@ -4,12 +4,18 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ZombieGameSystem {
+	private final int MOVING = 1;
+	private final int EXIT = 2;
+
 	private static Scanner scan = new Scanner(System.in);
 	private static Random ran = new Random();
 
 	private static Human human = new Human();
 	private static Normal normal = new Normal();
 	private static Boss boss = new Boss();
+
+	private boolean isrun = true;
+	private int potionCount = 3;
 
 	private void ZombieGameSystem() {
 
@@ -40,7 +46,35 @@ public class ZombieGameSystem {
 	}
 
 	public void run() {
-		int num = inputNumber("숫자입력");
+		while (isrun) {
+			menu();
+
+		}
+	}
+
+	private void menu() {
+		System.out.println("1.이동하기");
+		System.out.println("2.종료하기");
+		int select = inputNumber("메뉴 선택");
+
+		if (select < 1 || select > 2) {
+			System.out.println("메뉴 잘못입력했습니다!");
+			return;
+		}
+
+		if (select == MOVING) {
+			human.moving(human);
+		} else if (select == EXIT) {
+			System.out.println("좀비게임 종료 합니다!");
+			isrun = false;
+		}
+	}
+
+	private int randomNumber() {
+		int ranNum = ran.nextInt(5) + 1; // 1 2 3 4 5
+											// 1~3 이동 | 4 좀비 | 5 포션
+
+		return ranNum;
 	}
 
 	private int inputNumber(String msg) {
