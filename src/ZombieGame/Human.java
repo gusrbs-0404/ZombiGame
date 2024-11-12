5b74552eba49e82e0c5480dd270a91575ee934f7
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-12 11:32:25 +0900

    Modify upgrade() 공격력 10 -> 5

--- src/ZombieGame/Human.java
+++ src/ZombieGame/Human.java
@@ -3,6 +3,8 @@ package ZombieGame;
 public class Human extends Unit implements Potion, Moving {
 
 	private int totalMoving = 0;
+	private int upgradeCount = 1;
+	private int attackUpgrade = 0;
 
 	Human() {
 		super("길동이", 200);
@@ -10,7 +12,7 @@ public class Human extends Unit implements Potion, Moving {
 
 	@Override
 	public void attack(Unit unit) {
-		int attack = ZombieGameSystem.ranAttack();
+		int attack = ZombieGameSystem.ranAttack() + attackUpgrade;
 		String masg = String.format("%s가 %s에게 %d만큼 공격했다!!!!", name, unit.name, attack);
 		System.out.println(masg);
 
@@ -21,24 +23,21 @@ public class Human extends Unit implements Potion, Moving {
 
 	@Override
 	public void potion(Human human) {
-		if (human instanceof Human) {
-			Human target = (Human) human;
 
-			int ranHp = ZombieGameSystem.ranHp();
+		int ranHp = ZombieGameSystem.ranHp();
 
-			System.out.printf("포션을 먹었다! %d만큼 획복한다!!\n", ranHp);
-			while (ranHp > 0 && target.hp < target.MAX_HP) {
-				System.out.println(target + "|| [HP] 회복중");
-				target.hp++;
-				ranHp--;
-				try {
-					Thread.sleep(300);
-				} catch (Exception e) {
-				}
+		System.out.printf("포션을 먹었다! %d만큼 획복한다!!\n", ranHp);
+		while (ranHp >= 0 && human.hp <= human.MAX_HP) {
+			System.out.println(human + "|| [HP] 회복중");
+			human.hp++;
+			ranHp--;
+			try {
+				Thread.sleep(300);
+			} catch (Exception e) {
 			}
-
-			System.out.println("<<< 회복 완료 >>> ");
 		}
+
+		System.out.println("<<< 회복 완료 >>> ");
 	}
 
 	@Override
@@ -48,6 +47,19 @@ public class Human extends Unit implements Potion, Moving {
 
 		String masg = String.format("%s가 %d만큼 이동했다.\n총 이동거리 : %d", name, moving, totalMoving);
 		System.out.println(masg);
+	}
+
+	public void upgrade(Human human) {
+		if (totalMoving / 10 == upgradeCount) {
+			System.out.println(totalMoving / 10);
+			String masg = String.format("%s의 [체력 : 10 | 공격력 : 5] 증가 합니다!", name);
+			System.out.println(masg);
+
+			human.MAX_HP += 10;
+			upgradeCount++;
+			attackUpgrade += 5;
+			System.out.println(human);
+		}
 	}
 
 }
